fc634312ab94cba4a8c907316d9bb47fca4d7eb9
bob <bob@APCSA>
2017-10-18 14:03:10 -0700

    Update GasMileage

--- src/GasMileage.java
+++ src/GasMileage.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
 public class GasMileage
 {
-
+	private ArrayList<Trip> gas;
+	public GasMileage()
+	{
+		gas = new ArrayList<Trip>();
+	}
+	public void addTrip(Trip trip)
+	{
+		gas.add(trip);
+	}
+	public double gasMileage()
+	{
+		int tmiles = 0;
+		int tgallons = 0;
+		For(Trip gas : list)
+		{
+			tmiles = 
+			tgallons = 
+		}
+	}
+	
+	
+	
+	
+	
 }
+
