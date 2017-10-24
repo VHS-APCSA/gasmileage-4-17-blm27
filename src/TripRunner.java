a180d889e8604ddf201edb40e7832afbfbaea740
littl <littl@DESKTOP-IK0GM3J.attlocal.net>
2017-10-23 21:20:15 -0700

    Added scanner to TripRunner

--- src/TripRunner.java
+++ src/TripRunner.java
@@ -1,4 +1,17 @@
-
-public class TripRunner {
-
+import java.util.Scanner;
+public class TripRunner 
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		GasMileage travel = new GasMileage();
+		int miles = 0;
+		int gallons = 0;
+		while( >= 0)
+		{
+			System.out.println("Enter miles and then gallons." + "Enter -1 to quit loop.\n");
+			
+		}
+		
+	}
 }
