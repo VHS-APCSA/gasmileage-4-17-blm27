23f77392507b98dd455e6cf5356fe26fa53f6db5
bob <bob@APCSA>
2017-10-24 14:49:19 -0700

    Added while loop that takes input.

--- src/TripRunner.java
+++ src/TripRunner.java
@@ -4,14 +4,22 @@ public class TripRunner
 	public static void main(String[] args)
 	{
 		Scanner input = new Scanner(System.in);
-		GasMileage travel = new GasMileage();
+		GasMileage gas = new GasMileage();
 		int miles = 0;
 		int gallons = 0;
-		while( >= 0)
+		while(miles >= 0 )
 		{
 			System.out.println("Enter miles and then gallons." + "Enter -1 to quit loop.\n");
-			
+			miles = input.nextInt();
+			gallons = input.nextInt();
+			if(miles >= 0)
+			{
+				gas.addTrip(new Trip(miles, gallons));
+				System.out.println((double)miles/gallons);
+			}
 		}
+		System.out.println(gas);
+		
 		
 	}
 }
